c138a956d148bc779a7eb098479e8d970dec059e
esterekarklis <ester.karklis@lumusit.com.br>
2026-08-19 17:37:02 -0300

    test: primeira alteração para concorrência

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Build atualizado após merge na develop
+          Teste de concorrência 1
         </p>
       </div>
       <p className="read-the-docs">
